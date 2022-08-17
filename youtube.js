a90345d3f1c73fccbf32c03ea767c7d0155a0fc3
Allen <allencch@hotmail.com>
2022-08-18 01:25:24 +0800

    Youtube improve

--- youtube.js
+++ youtube.js
@@ -2,8 +2,14 @@
   const url = document.querySelector('meta[property=al\\:web\\:url]').content;
   const elem = document.querySelector('#channel-name');
 
+  const existing = document.querySelector('#bookmarklet-channel');
+  if (existing) {
+    existing.remove();
+  }
+
   const container = document.createElement('div');
   container.setAttribute('style', 'background:white;padding:4px');
+  container.setAttribute('id', 'bookmarklet-channel');
   container.innerHTML = new URL(url).pathname;
 
   elem.parentElement.append(container);
