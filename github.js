555a78093316a67040b538e9d804d31ea27739b8
Allen <allencch@hotmail.com>
2026-02-19 01:37:03 +0800

    fix: github update

--- github.js
+++ github.js
@@ -1,5 +1,5 @@
 (() => {
-  const elem = document.querySelector('.gh-header-title > span:nth-child(2)');
+  const elem = document.querySelector('[data-component="TitleArea"] span:nth-child(2)');
   const link = document.createElement('a');
   link.setAttribute('href', window.location);
   link.innerHTML = elem.innerHTML.replace('#', '');
